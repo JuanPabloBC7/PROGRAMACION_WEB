992242c803176dc86f345166a6875b2ad0c39628
JuanPabloBC7 <juan_pablo2410@hotmail.com>
2020-09-15 16:57:42 -0600

    Empezamos de 0 de nuevo

--- PRODUCTOS/src/app/app.component.ts
+++ PRODUCTOS/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Class_Producto } from './Models/Producto/Producto';
 
 @Component({
   selector: 'app-root',
@@ -7,34 +6,5 @@ import { Class_Producto } from './Models/Producto/Producto';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public tituloAngJPB = 'Mi primera pagina en Angular';
-  
-  productoArreglo: Class_Producto[] = [
-    { ID: 1, Nombre: "Sentry", Descripcion: "Tabletas", Laboratorio: "BAYER", Origen: "Extranjero", Fecha_Vencimiento: "06/06/2020", Vigente: true, Precio: 25 },
-    { ID: 2, Nombre: "Klein", Descripcion: "Gotas", Laboratorio: "BAYER", Origen: "Nacional", Fecha_Vencimiento: "06/09/2020", Vigente: true, Precio: 50 },
-    { ID: 3, Nombre: "Sentry", Descripcion: "Tabletas", Laboratorio: "BAYER", Origen: "Extranjero", Fecha_Vencimiento: "06/06/2020", Vigente: true, Precio: 25 },
-    { ID: 4, Nombre: "Aspirina", Descripcion: "Pastillas", Laboratorio: "BAYER", Origen: "Extranjero", Fecha_Vencimiento: "06/06/2020", Vigente: true, Precio: 15 },
-    { ID: 5, Nombre: "NyQuil Children", Descripcion: "Jarabe", Laboratorio: "VICKS", Origen: "Nacional", Fecha_Vencimiento: "06/06/2020", Vigente: true, Precio: 20 }
-  ];
-
-  selectedProducto: Class_Producto = new Class_Producto();
-
-  btn_AgregarOEditar_Click(){
-    if(this.selectedProducto.ID === 0){
-      this.selectedProducto.ID = this.productoArreglo.length + 1;
-      this.productoArreglo.push(this.selectedProducto);
-    }    
-    this.selectedProducto = new Class_Producto();
-  }
-
-  tbl_Editar_Click(Producto: Class_Producto){
-    this.selectedProducto = Producto;
-  }
-
-  btn_Eliminar_Click(){
-    if(confirm('Seguro de querer eliminarlo?')){
-      this.productoArreglo = this.productoArreglo.filter(X => X != this.selectedProducto);
-      this.selectedProducto = new Class_Producto();
-    }
-  }
+  title = 'PRODUCTOS';
 }
