bf64934c8ae7f5ac6eaa16ced25b92b0f3bd6d3f
JuanPabloBC7 <juan_pablo2410@hotmail.com>
2020-09-16 23:01:28 -0600

    Modificacion de tabla a una tabla mas sencilla

--- PRODUCTOS/src/app/components/menu/administrar-productos/administrar-productos.component.ts
+++ PRODUCTOS/src/app/components/menu/administrar-productos/administrar-productos.component.ts
@@ -4,63 +4,53 @@ import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 
-export interface TablaData {
-  id: string;
+export interface ProductosObject {
+  ID: number;
   ColumnaNombre: string;
   ColumnaTipo: string;
   ColumnaOrigen: string;
   ColumnaPrecio: string;
 }
 
-/** Constants used to fill up our data base. */
-const L_ARRAY_NOMBRE: string[] = [
-  'ASPIRINA', 'OMEPRAZOL', 'LEXOTIROXINA SODICA', 'RAMIPRIL', 'WARFARINA SODICA', 'SALBUTAMOL', 'PARACETAMOL', 'BENDROFLUMETIAZIDA',
-  'MORFINA', 'PROMETAZINA', 'IBOPRUFENO', 'DEXAMETASONA', 'EPINEFRINA', 'NALOXONA', 'NITRITO DE SODIO', 'PENICILINA', 'PENICILAMINA',
-  'PIRANTEL', 'PRAZICUANTEL', 'AMPICILINA'
-]
-const L_ARRAY_TIPO: string[] = [
-  'PASTILLA', 'CAPSULA', 'LIQUIDA', 'JARABE', 'INYECTADA'
-]
-const L_ARRAY_PAIS: string[] = [
-  'ALEMANIA', 'FRANCIA', 'BRASIL', 'ITALIA', 'COSTA RICA', 'ARGENTINA', 'GUATEMALA', 'BELGICA', 'CANADA', 'CHILE', 'COLOMBIA', 
-  'REPUBLICA DOMINICANA', 'CHINA', 'JAPON', 'EL SALVADOR', 'ESPAÑA', 'HONDURAS', 'MEXICO', 'ESTADOS UNIDOS', 'PORTUGAL'
-]
-/**
- * @title Data table with sorting, pagination, and filtering.
- */
+const ElementosTabla: ProductosObject[] = [
+  { ID: 1, ColumnaNombre: 'ASPIRINA', ColumnaTipo: 'PASTILLA', ColumnaOrigen: 'ALEMANIA', ColumnaPrecio: '8.34' },
+  { ID: 2, ColumnaNombre: 'OMEPRAZOL', ColumnaTipo: 'CAPSULA', ColumnaOrigen: 'FRANCIA', ColumnaPrecio: '49.66' },
+  { ID: 3, ColumnaNombre: 'LEXOTIROXINA SODICA', ColumnaTipo: 'LIQUIDA', ColumnaOrigen: 'BRASIL', ColumnaPrecio: '16.26' },
+  { ID: 4, ColumnaNombre: 'RAMIPRIL', ColumnaTipo: 'JARABE', ColumnaOrigen: 'ITALIA', ColumnaPrecio: '43.13' },
+  { ID: 5, ColumnaNombre: 'WARFARINA SODICA', ColumnaTipo: 'INYECTADA', ColumnaOrigen: 'COSTA RICA', ColumnaPrecio: '74.86' },
+  { ID: 6, ColumnaNombre: 'SALBUTAMOL', ColumnaTipo: 'PASTILLA', ColumnaOrigen: 'ARGENTINA', ColumnaPrecio: '42.38' },
+  { ID: 7, ColumnaNombre: 'PARACETAMOL', ColumnaTipo: 'CAPSULA', ColumnaOrigen: 'GUATEMALA', ColumnaPrecio: '81.42' },
+  { ID: 8, ColumnaNombre: 'BENDROFLUMETIAZIDA', ColumnaTipo: 'LIQUIDA', ColumnaOrigen: 'BELGICA', ColumnaPrecio: '89.95' },
+  { ID: 9, ColumnaNombre: 'PRAZICUANTEL', ColumnaTipo: 'JARABE', ColumnaOrigen: 'CANADA', ColumnaPrecio: '40.32' },
+  { ID: 10, ColumnaNombre: 'AMPICILINA', ColumnaTipo: 'INYECTADA', ColumnaOrigen: 'CHILE', ColumnaPrecio: '7.29' },
+  { ID: 11, ColumnaNombre: 'MORFINA', ColumnaTipo: 'PASTILLA', ColumnaOrigen: 'COLOMBIA', ColumnaPrecio: '8.56' },
+  { ID: 12, ColumnaNombre: 'PIRANTEL', ColumnaTipo: 'CAPSULA', ColumnaOrigen: 'REPUBLICA DOMINICANA', ColumnaPrecio: '65.41' },
+  { ID: 13, ColumnaNombre: 'PROMETAZINA', ColumnaTipo: 'LIQUIDA', ColumnaOrigen: 'CHINA', ColumnaPrecio: '73.68' },
+  { ID: 14, ColumnaNombre: 'IBOPRUFENO', ColumnaTipo: 'JARABE', ColumnaOrigen: 'JAPON', ColumnaPrecio: '43.74' },
+  { ID: 15, ColumnaNombre: 'DEXAMETASONA', ColumnaTipo: 'INYECTADA', ColumnaOrigen: 'EL SALVADOR', ColumnaPrecio: '83.39' },
+  { ID: 16, ColumnaNombre: 'EPINEFRINA', ColumnaTipo: 'PASTILLA', ColumnaOrigen: 'ESPAÑA', ColumnaPrecio: '98.29' },
+  { ID: 17, ColumnaNombre: 'NALOXONA', ColumnaTipo: 'CAPSULA', ColumnaOrigen: 'HONDURAS', ColumnaPrecio: '35.19' },
+  { ID: 18, ColumnaNombre: 'NITRITO DE SODIO', ColumnaTipo: 'LIQUIDA', ColumnaOrigen: 'MEXICO', ColumnaPrecio: '26.51' },
+  { ID: 19, ColumnaNombre: 'PENICILINA', ColumnaTipo: 'JARABE', ColumnaOrigen: 'ESTADOS UNIDOS', ColumnaPrecio: '49.88' },
+  { ID: 20, ColumnaNombre: 'PENICILAMINA', ColumnaTipo: 'INYECTADA', ColumnaOrigen: 'PORTUGAL', ColumnaPrecio: '15.40' },
+];
+
 @Component({
   selector: 'app-administrar-productos',
   templateUrl: './administrar-productos.component.html',
   styleUrls: ['./administrar-productos.component.css']
 })
 export class AdministrarProductosComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'ColumnaNombreFE', 'ColumnaTipoFE', 'ColumnaOrigenFE', 'ColumnaPrecioFE'];
-  dataSource: MatTableDataSource<TablaData>;
+  displayedColumns: string[] = ['ColumnaIDFE', 'ColumnaNombreFE', 'ColumnaTipoFE', 'ColumnaOrigenFE', 'ColumnaPrecioFE'];
+  dataSource = new MatTableDataSource<ProductosObject>(ElementosTabla);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
-
-  constructor(private router: Router) { 
-    // Create 100 users
-    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
-
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
-  }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
   }
 
-  MetodoFiltrado(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -68,19 +58,4 @@ export class AdministrarProductosComponent implements OnInit {
   NavegarHaciaSalir(){
     this.router.navigate(['/Principal'])
   }
-}
-
-/** Builds and returns a new User. */
-function createNewUser(id: number): TablaData {
-  const Arreglonombre = L_ARRAY_NOMBRE[Math.round(Math.random() * (L_ARRAY_NOMBRE.length - 1))];
-  const ArregloTipo = L_ARRAY_TIPO[Math.round(Math.random() * (L_ARRAY_TIPO.length - 1))];
-  const ArregloOrigen = L_ARRAY_PAIS[Math.round(Math.random() * (L_ARRAY_PAIS.length - 1))];
-
-  return {
-    id: id.toString(),
-    ColumnaNombre: Arreglonombre,
-    ColumnaTipo: ArregloTipo,
-    ColumnaOrigen: ArregloOrigen,
-    ColumnaPrecio: Math.round(Math.random() * 1000).toString() + '.' + Math.round(Math.random() * 100).toString()
-  };
 }
